ccee483b544995c93e993d3ebf648de03c889ffe
Xhst <82611659+Xhst@users.noreply.github.com>
2025-03-03 00:43:36 +0100

    feat: app

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+import "./App.css";
+import logo from "./assets/images/infty.svg";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Footer from "./components/Footer";
+
+const Navbar = () => {
+  const location = useLocation(); 
+
+  let navClassName = "navbar navbar-expand-md fixed-top d-main-nav ";
+  navClassName += location.pathname === "/" ? "d-tgbg" : "";
+
+  return (
+    <nav className={navClassName}>
+      <div className="container-fluid px-4">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <button
+          className="navbar-toggler collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="bi bi-list open-menu"></i>
+          <i className="bi bi-x-lg close-menu"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/Xhst"
+              >
+                Github <i className="bi bi-arrow-up-right"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects">
+                Projects
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
